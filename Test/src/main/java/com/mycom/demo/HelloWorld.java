3969c4b85ed8beb0647f1609aa56b8fe298bf553
Gavin House <gavin@house.com>
2020-02-17 11:42:04 -0600

    Hello amdocs!

--- Test/src/main/java/com/mycom/demo/HelloWorld.java
+++ Test/src/main/java/com/mycom/demo/HelloWorld.java
@@ -3,7 +3,7 @@ package com.mycom.demo;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! amdocs");
     }
 
 }
