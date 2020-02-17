5a91bf716a34eb511bf6f44685e047521110d8a2
Gavin House <gavin@house.com>
2020-02-17 14:05:39 -0600

    many log4j things

--- Test/src/main/java/com/mycom/demo/HelloWorld.java
+++ Test/src/main/java/com/mycom/demo/HelloWorld.java
@@ -1,9 +1,16 @@
 package com.mycom.demo;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
 
+    private static Logger LOG = Logger.getLogger("HelloWorld");
+
     public static void main(String[] args) {
+
         System.out.println("Hello World! amdocs dallas");
+        LOG.info("This is an info message");
+
     }
 
 }
